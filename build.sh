7e79889efed6e16ce4ccfc0edeb68f0cf74753b0
Nagasai48 <nagasai8748@gmail.com>
2026-03-22 20:28:26 +0530

    Fixed Railway model loading issue using gdown

--- build.sh
+++ build.sh
@@ -4,5 +4,29 @@ set -o errexit
 
 pip install -r requirements.txt
 
+# Download model from Google Drive
+# Replace 'YOUR_GOOGLE_DRIVE_FILE_ID' below with the actual file ID from your shareable link!
+python -c "
+import os
+import gdown
+
+file_id = '1N9-cnWPOs2z0VGplxdJU1nuBQYoLXZHf'
+dest_path = 'models/resnet34_model.h5'
+
+if not os.path.exists('models'):
+    os.makedirs('models')
+
+print('Checking if model file exists and is valid...')
+if not os.path.exists(dest_path) or os.path.getsize(dest_path) < 10000000:
+    print('Downloading resnet34_model.h5 from Google Drive...')
+    try:
+        url = f'https://drive.google.com/uc?id={file_id}'
+        gdown.download(url, dest_path, quiet=False)
+    except Exception as e:
+        print(f'Failed to download model: {e}')
+else:
+    print('Model already exists and seems valid.')
+"
+
 python manage.py collectstatic --no-input
 python manage.py migrate
